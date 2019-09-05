3657b081fe30f97e2baf737aecc2dea3d6f95579
Jorge Hecherat <hecherat@gmail.com>
2019-09-05 19:10:37 -0300

    Github icon, + css

--- github-react/src/Components/Header/index.js
+++ github-react/src/Components/Header/index.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Button from '../Button/index.js';
 import './header.css'
+import Logo from '../../assets/imgs/github.png'
 
 const Header = () => {
 
   return (
     <>
       <div className="fixed-menu-box">
-        <p className="menu-title">Trainees Front-end</p>
+        <div className="logo-box">
+          <Link to={"/"}>
+            <img className="github-icon"src={Logo} alt="Github Logo" />
+            <p className="menu-title">Trainees Front-end</p>
+          </Link>
+        </div>
         <div className="box-fixed-menu-btn">
           <Link to={"/"}><Button>home</Button></Link>
           <Link to={"/equipe"}><Button>equipe</Button></Link>
